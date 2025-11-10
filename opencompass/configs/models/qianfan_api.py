c8615fb13d47c07210993a4af813dcff8baf3515
Rick21-ss <2735886438@qq.com>
2025-11-10 10:48:24 +0800

    修正：正确更新Qwen和千帆API的配置文件

--- opencompass/configs/models/qianfan_api.py
+++ opencompass/configs/models/qianfan_api.py
@@ -7,13 +7,11 @@ models = [
         path='ernie-x1.1-preview',
         key='bce-v3/ALTAK-uARdaVcSqYLhSSlIovGNJ/7206fbda503b0f144e8d1e03626b165152e54c94',
         openai_api_base='https://qianfan.baidubce.com/v2',
-        query_per_second=1,
-        batch_size=4,
+        query_per_second=16,
+        batch_size=128,
         tokenizer_path='gpt-4',
         max_out_len=16384,
         max_seq_len=32768,
-        retry=0,  # 遇到错误不重试
-        continue_on_error=True,  # 遇到错误继续处理下一个
         temperature=0.6,
     )
 ]
--- opencompass/configs/models/qwen_api.py
+++ opencompass/configs/models/qwen_api.py
@@ -7,13 +7,11 @@ models = [
         path='qwen3-max-preview',
         key='sk-b1ba35174a46487086152a4d439307fc',
         openai_api_base='https://dashscope.aliyuncs.com/compatible-mode/v1',
-        query_per_second=1,
-        batch_size=4,
+        query_per_second=16,
+        batch_size=128,
         tokenizer_path='gpt-4',
         max_out_len=4096,
         max_seq_len=32768,
-        retry=0,  # 遇到错误不重试
-        continue_on_error=True,  # 遇到错误继续处理下一个
         temperature=0.6,
     )
 ]
